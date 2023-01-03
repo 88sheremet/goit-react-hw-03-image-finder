7035680cc90e10bb4315d19958ccc94cec25b6d8
Oleksandr Sheremet <88sheremet@gmail.com>
2023-01-03 17:34:49 +0200

    add and change

---
+++ src/components/App/App.jsx
@@ -0,0 +1,8 @@
+import { SearchBar } from 'components/Searchbar/Searchbar';
+import css from '../App/App.module.css';
+
+export const App = () => {
+  return <div className={css.App}>
+    <SearchBar/>
+  </div>;
+};
